f5137365af9517c161e6ee65aeb59cab1b9929fe
Tnatiel <117045287+Tnatiel@users.noreply.github.com>
2023-01-16 01:11:03 +0200

    Modified input slice, added className field and function to creact the inputs objects

--- src/redux/features/InputState.ts
+++ src/redux/features/InputState.ts
@@ -1,30 +1,28 @@
 import { createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 
-export interface InputState {
+export interface InputBox {
     id: number, 
-    value: string
+    value: string,
+    className: string
 }
 
 export interface InputsState {
-    rows: InputState[][],
+    rows: InputBox[][],
     currentInput: number,
 };
 
 
+const createInputs = (ids: number[]) => ids.map( id => ({id, value: '', className: ''}));
 
-
-
-
-
-
-const row1 = [0, 1, 2, 3, 4].map( id => ({id, value: ''}));
-const row2 = [5, 6, 7, 8, 9].map( id => ({id, value: ''}));
-const row3 = [10, 11, 12, 13, 14].map( id => ({id, value: ''}));
-const row4 = [15, 16, 17, 18, 19].map( id => ({id, value: ''}));
-const row5 = [20, 21, 22, 23, 24].map( id => ({id, value: ''}));
-const row6 = [25, 26, 27, 28, 29].map( id => ({id, value: ''}));
+const row1 = createInputs([0, 1, 2, 3, 4])
+const row2 = createInputs([5, 6, 7, 8, 9])
+const row3 = createInputs([10, 11, 12, 13, 14])
+const row4 = createInputs([15, 16, 17, 18, 19])
+const row5 = createInputs([20, 21, 22, 23, 24])
+const row6 = createInputs([25, 26, 27, 28, 29])
 const rows = [row1, row2, row3, row4, row5, row6]
+
 const initialState: InputsState = {
     rows,
     currentInput: 0,
@@ -35,14 +33,11 @@ const inputSlice = createSlice({
     initialState,
     reducers: {
         updateInputValue(state, action: PayloadAction<{inputIndex: number, value: string}>) {
-
             state.rows[action.payload.inputIndex][state.currentInput].value = action.payload.value;
         },
         updateNextInput(state) {
             state.currentInput += 1
         },
-        
-
     }
 });
 
