19104b013ce9b0d3c58b671caed8c946f9782faa
Tnatiel <117045287+Tnatiel@users.noreply.github.com>
2023-01-16 09:52:48 +0200

    Canceled use with inputBox and keyboardButton components

--- src/wordle-components/InputRow.tsx
+++ src/wordle-components/InputRow.tsx
@@ -4,22 +4,53 @@ import { useInputRow } from "../custom-hooks/useInputRow";
 import { useAppDispatch, useAppSelector } from "../redux/app/hooks";
 import { updateNextInput, updateInputValue } from "../redux/features/InputState";
 import { addCorrectLetter, addWrongLetter, addPresentLetter } from "../redux/features/LettersState"; 
-import { useRef, useMemo } from "react";
-import { InputBox } from "./InputBox";
 import { useInputRef } from "../custom-hooks/useInputRefs";
+import { useEffect } from 'react';
+
 
 export function InputRow({rowIndex: rowNumber}: {rowIndex: number}) {
     const { inputs } = useInputRow(rowNumber);
+    const currentInput = useAppSelector(state => state.inputs.currentInput)
+    const inputsRefs = useInputRef();
+    const dispatch = useAppDispatch();
+    const word = useAppSelector(state => state.game.word)
+    
+    useEffect(() => {
+        if (currentInput % 5 === 0 && currentInput !== 0) return
+        if (currentInput !== undefined) {
+            inputsRefs[currentInput].current?.focus();
+        }
+    }, [currentInput, inputsRefs]);
 
+    const handleInputChange = (e: Partial<Event>) => {
+        const value = (e.target as HTMLInputElement).value;
+        checkAndUpdateLetters(value);
+        dispatch(updateNextInput())
+
+    };
+
+    const checkAndUpdateLetters = (value: string) => {
+        if (value === word[currentInput]) {
+            dispatch(addCorrectLetter(value));
+        } else if (word.includes(value)) {
+            dispatch(addPresentLetter(value));
+        } else {
+            dispatch(addWrongLetter(value));
+        }
+    };
 
     return (
         <div className="input-row">
             {inputs.map(input => (
-                <InputBox
-                    id={input.id}
-                    value={input.value}
-                    key={input.id}
-                    inputClassName={input.className}
+                <input
+                id={input.id.toString()}
+                className={`ur-input ${input.className}`}
+                autoComplete={'off'}
+                maxLength={1}
+                onInput={(e) => handleInputChange(e)}
+                ref={inputsRefs[input.id]}
+                defaultValue={input.value}
+                disabled={input.id === currentInput ? false : true}
                 />
             ))}
         </div>
