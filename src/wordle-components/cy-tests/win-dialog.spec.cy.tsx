7935046b368924b772825f7d4360167c236ecb12
Tnatiel <117045287+Tnatiel@users.noreply.github.com>
2023-01-27 03:05:21 +0200

    Added winDialog tests

---
+++ src/wordle-components/cy-tests/win-dialog.spec.cy.tsx
@@ -0,0 +1,38 @@
+import { Provider } from "react-redux";
+import { setWinDialog } from "redux/features/DialogState";
+import { setWin } from "redux/features/GameState";
+import { WinDialog } from "wordle-components/dialog/WinDialog";
+import { initializeTests } from "./keyboard.spec.cy";
+
+
+
+describe('WinDialog', () => {
+
+    beforeEach(() => {
+        mockStore.dispatch(setWinDialog(true))
+        cy.mount(
+            <Provider store={mockStore} >
+                    <WinDialog/>
+                 </Provider>
+            )
+    })
+    const {mockStore} = initializeTests()
+    it('should render the dialog', () => {
+        
+    });
+    it('modal header should be "You Guessed The Wordle!"', () => {
+        cy.get('h3').should('have.text', 'You Guessed The Wordle!')
+    });
+    it('modal main content should be "🎉 Congrats!! 🎉"', () => {
+        
+        cy.get('h1').should('have.text', '🎉 Congrats!! 🎉')
+    });
+    it('should have button with text "Yay!"', () => {
+        
+        cy.get('button').should('have.text', 'Yay!')
+    });
+    it('should stop render', () => {
+        mockStore.dispatch(setWinDialog(false))
+        cy.get('button').should('be.', 'onClick')
+    });
+})
