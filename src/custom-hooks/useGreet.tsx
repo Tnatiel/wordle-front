de4c9ceb487a388adbccb1fba84a69bf9971a26f
johnny shafir <97397410+Josnon@users.noreply.github.com>
2023-01-31 05:43:56 +0200

    as

--- src/custom-hooks/useGreet.tsx
+++ src/custom-hooks/useGreet.tsx
@@ -1,38 +1,46 @@
-import { useRef, useState } from 'react';
-
+import { useRef, useState } from "react";
 
 export const useGreet = () => {
-
-    const formRef = useRef<HTMLFormElement>(null)
-    const [logout, setLogout] = useState(false);
-    const addUserData = () => {
-        if (formRef.current) {
-          const userData  = new FormData(formRef.current);
-          const valuesObj = Object.fromEntries(userData.entries())
-          localStorage.setItem('name', valuesObj.name.toString())
-          localStorage.setItem('email', valuesObj.email.toString())
-          console.log('local storage: ', localStorage)
-          setLogout(false);
-      }
+  const formRef = useRef<HTMLFormElement>(null);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [logout, setLogout] = useState(false);
+  const addUserData = () => {
+    if (formRef.current) {
+      const userData = new FormData(formRef.current);
+      const valuesObj = Object.fromEntries(userData.entries());
+      localStorage.setItem("name", valuesObj.name.toString());
+      localStorage.setItem("email", valuesObj.email.toString());
+      console.log("local storage: ", localStorage);
+      setLogout(false);
     }
-    
-    const logoutUser = () => {
-      localStorage.clear();
-      setLogout(true)
-    }
+  };
 
-    const handleSubmit = () => {
-      addUserData();
-      handleSignInClose()
-    }
+  const logoutUser = () => {
+    localStorage.clear();
+    setLogout(true);
+  };
 
-    const [showSignIn, setShowSignIn] = useState(false);
-    const handleSignInClose = (): void => setShowSignIn(false);
-    const handleSignInShow = (): void => setShowSignIn(true);
+  const handleSubmit = () => {
+    addUserData();
+    handleSignInClose();
+  };
 
-    const userName = localStorage.getItem('name') !== null ?
-    localStorage.getItem('name') : 'Guest';
+  const [showSignIn, setShowSignIn] = useState(false);
+  const handleSignInClose = (): void => setShowSignIn(false);
+  const handleSignInShow = (): void => setShowSignIn(true);
 
+  const userName =
+    localStorage.getItem("name") !== null
+      ? localStorage.getItem("name")
+      : "Guest";
 
-      return { userName, handleSubmit, formRef,  logoutUser, handleSignInShow, handleSignInClose, showSignIn }
-}
+  return {
+    userName,
+    handleSubmit,
+    formRef,
+    logoutUser,
+    handleSignInShow,
+    handleSignInClose,
+    showSignIn,
+  };
+};
