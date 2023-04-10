575cb322eb9c8a1ffab13c9f7d421fdc9db0b800
Tnatiel <117045287+Tnatiel@users.noreply.github.com>
2023-04-10 06:56:15 +0300

    Fixed navbar width issue

--- src/main-components/NavBar.tsx
+++ src/main-components/NavBar.tsx
@@ -27,7 +27,7 @@ function NavBar({ showLogout, openRegisterModal, openInstructionsModal, openSign
 
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
+      {/* <Container> */}
         <Navbar.Brand><Link cy-data="wordle-btn" className="navbar-brand" to='wordle'>Wordle</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -57,7 +57,7 @@ function NavBar({ showLogout, openRegisterModal, openInstructionsModal, openSign
             </Button>
           </Nav>
         </Navbar.Collapse>
-      </Container>
+      {/* </Container> */}
     </Navbar>
   );
 }
